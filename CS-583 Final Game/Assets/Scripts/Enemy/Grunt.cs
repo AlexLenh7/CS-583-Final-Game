d4ced322ae7a26e14e0e6b94bc2e19124d56ce60
cervantesryan <cervantes.ryan.1@gmail.com>
2024-11-25 23:01:37 -0800

    Created Grunt Enemy
    Created a scene to test out the Grunt enemy. Copied the player prefab and changed the color. Added the script for it and now it chases the player. Will add other enemies that shoot.

---
+++ CS-583 Final Game/Assets/Scripts/Enemy/Grunt.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grunt : MonoBehaviour
+{
+    // hp of the grunt
+    public int health = 50;
+
+    // move speed of the grunt
+    public float moveSpeed = 3f;
+
+    // use to find player
+    private Transform player;
+
+    // rotation speed of the grunt
+    public float rotationSpeed = 10f;
+
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        MoveToPlayer();
+        RotateTowardsPlayer();
+    }
+
+    void MoveToPlayer()
+{
+    // Find direction to player
+    Vector3 moveDirection = (player.position - transform.position).normalized;
+
+    // Move enemy
+    transform.position += moveDirection * moveSpeed * Time.deltaTime;
+}
+
+void RotateTowardsPlayer()
+{
+    // Find direction to player
+    Vector3 direction = (player.position - transform.position).normalized;
+
+    // Calculate rotation towards player
+    Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+    // Smoothly rotate towards the player
+    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+}
+}
