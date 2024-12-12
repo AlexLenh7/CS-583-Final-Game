aa81585bfb94d4344d64ef440926f14693a7e1c8
Jonnugz <jn8865@gmail.com>
2024-12-11 17:31:02 -0800

    Camera Movement
    - Implemented ew camera script
    - Adjusted player cursor tracking appropriately

--- CS-583 Final Game/Assets/Scripts/Camera/Camera.cs
+++ CS-583 Final Game/Assets/Scripts/Camera/Camera.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovment : MonoBehaviour
+public class Camera : MonoBehaviour
 {
     //movement variables
     public Transform player;
     public float smooth = 0.1225f;
+    public float height = 12;
 
-    //idk
     private Vector3 velocity = Vector3.zero;
 
+    //Methods
     void Update()
     {
-        //idk
+
         Vector3 pos = new Vector3();
+
+        transform.rotation = Quaternion.Euler(50, 0, 0);
 
         //movement
         pos.x = player.position.x;
-        pos.z = player.position.z + -5.5f;
-        pos.y = player.position.y + 10f;
+        pos.z = player.position.z + -10f;
+        pos.y = player.position.y + height;
 
         transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
 
