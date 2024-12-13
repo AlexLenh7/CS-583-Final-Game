81207758a71c3d825be0996965ce14096d2dc8ce
Jonnugz <jn8865@gmail.com>
2024-12-12 16:15:42 -0800

    Added sound effect for Title Screen
    Sound Manager Script implemented

--- CS-583 Final Game/Assets/Scripts/UI/TitleScreen.cs
+++ CS-583 Final Game/Assets/Scripts/UI/TitleScreen.cs
@@ -5,14 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+    [SerializeField] private AudioClip interactSound;
+
     public void Play()
     {
         SceneManager.LoadScene("Level 1");
         Time.timeScale = 1f;
+        SoundManager.instance.PlaySound(interactSound);
     }
 
     public void Quit()
     {
+        SoundManager.instance.PlaySound(interactSound);
         Application.Quit();
     }
 }
